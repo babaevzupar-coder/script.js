c25daf795f3e5f29d514ba14910335c170118091
babaevzupar-coder <babaevzupar@gmail.com>
2026-01-14 00:18:07 +0500

    Add home work

--- 1/script.js
+++ 1/script.js
@@ -1,5 +1,29 @@
-let myName = "My name Zupar"
-let myAge = 27 
-let myCountre = "Uzbekistan"
+let myName = "Zupar";
+let myAge = 27;
+console.log("Меня зовут"+" " + myName, "Мне" + " " + myAge,  "Годиков" );
 
-console.log(myName, myAge, myCountre)
+// alert ("Привет Меня зовут"+" " +myName);
+// alert ("Мой возраст"+" " +myAge);
+
+// let hobby=prompt("Какое ваше любимое хобби");
+// alert (hobby + " " + "Это хорошее хобби"+"!")
+
+// let color=prompt("Какой ваш любимый цвет?");
+// alert (color + "Это многое говорит о вас!")
+
+let city ="Какой твой город?";
+let country = "Это где?";
+let year = "Какой сейчас год?";
+
+console.log(city+" "+"Мой Город Самарканд!" +" "+country, "Этот Город находится в Узбекисатне!"+" " +year+ " " +"Сейчас 2026 Год");
+// let pet=prompt("Какое ваше любтмое животное?");
+// alert(pet +" "+ "Это лучшее животное");
+
+let a =100;
+let b =50;
+let c =60;
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log(a-b)
+console.log(c-b)
