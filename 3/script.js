2cc27d42d70628ec484fe3dd24b854cde46b08a2
babaevzupar-coder <babaevzupar@gmail.com>
2026-01-25 23:40:11 +0500

    Add home work

--- 3/script.js
+++ 3/script.js
@@ -6,6 +6,28 @@ if (clientAge >= 18){
 	alert("Вы не совершеннолетний")
 }
 
+let money = Number(prompt('Сколько у тебя денег?'));
+let price = 5000
+
+if (money>= price){
+	alert("Покупка успешна")
+}else{
+	alert("У вас недостаточно денег")
+}
 
 
+// < 16 - Отказ 
+// 16-17 Ограниченный доступ
+// 18 - Полный доступ
 
+let age = Number(prompt("Возраст"))
+
+
+if (age < 16)
+	alert("Доступ запрещен")
+else if (age < 18)
+	alert("Доступ ограничен")
+ else if (age < 22)
+ 	alert("Ограниченый доступ")
+else 
+	alert("Полный доступ")
