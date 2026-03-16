1ab2a855edcc7caf6fc387ea99e4f9d048906b14
babaevzupar-coder <babaevzupar@gmail.com>
2026-03-16 22:12:13 +0500

    Add home work

---
+++ 5/script.js
@@ -0,0 +1,18 @@
+let price = Number(prompt("Цена товара"))
+let quantity = Number(prompt("Колличевство"))
+
+let total = price * quantity
+alert("Цена" + " " + total + " " + "Сум")
+
+
+// if (total >= 200000){
+// 	total * 0.2
+// }else if (total >= 100000){
+// 	total - discount
+// } alert("Скидка" )
+
+
+// if (total >= 200000){
+// 	let discount = total *0.2;
+// } total= total - discount
+// alert("Скидка 20%. Итог" + total)
---
+++ 6/script.js
@@ -0,0 +1,11 @@
+function aboutMe(name, country, ) {
+  alert("Привет, " + name + " " + " " + country);
+}
+
+// name — параметр
+
+// "Akbar" — значение параметра
+
+aboutMe("Зупар,", "Узбекистан.");
+
+
